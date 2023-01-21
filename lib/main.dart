d9578e038f177c66d19be648e86781a64a8c3e6b
Mehmet Semih BABACAN <55881663+koltukutsu@users.noreply.github.com>
2023-01-21 09:09:03 +0300

    Initiation

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:wakely/ui/main_app.dart';
+import 'package:wakely/ui/screens/main/main_screen.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const MainScreen());
 }
--- lib/ui/screens/main/main_screen.dart
+++ lib/ui/screens/main/main_screen.dart
@@ -5,6 +5,25 @@ class MainScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: Scaffold(
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Expanded(
+                flex: 1,
+                child: Container(
+                  margin: const EdgeInsets.all(20.0),
+                  color: Colors.purple,
+                )),
+            Expanded(
+              flex: 3,
+                child: TextButton(
+                    onPressed: () {}, child: const Text("Take the Data")))
+          ],
+        ),
+      ),
+      debugShowCheckedModeBanner: false,
+    );
   }
 }
