29f0d1b78ec1a8dea74c26173f9020f9ef25b33a
Mehmet Semih BABACAN <55881663+koltukutsu@users.noreply.github.com>
2023-01-30 17:56:17 +0300

    Update: added example spotify sdk code

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:wakely/ui/main_app.dart';
 import 'package:wakely/ui/screens/main/main_screen.dart';
 
-void main() {
-  runApp(const MainScreen());
+void main() async {
+  await dotenv.load(fileName: '.env');
+  runApp(const Home());
 }
